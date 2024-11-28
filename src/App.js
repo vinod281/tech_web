63615c9155f7bff41b8fead9343de22a7fb88554
Duleesha28455 <duleeshajayangani29@gmail.com>
2024-11-28 20:27:31 +0530

    Edit Navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import Notification from './components/Notification/Notification';
 import Home from './pages/Home/Home';
 import ProductOverview from './pages/ProductOverview/ProductOverview';
-
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   
 
   return (
     <>
-      
+    <Navbar/>  
     <Home/>
     <ProductOverview/>
 
