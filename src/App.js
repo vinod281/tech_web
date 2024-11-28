c9b8795c74752c316aa05d0d01e5ee660850a27c
vinod281 <57266038+vinod281@users.noreply.github.com>
2024-11-29 01:12:39 +0530

    edit home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import Home from './pages/Home/Home';
 import ProductOverview from './pages/ProductOverview/ProductOverview';
-import Navbar from './components/Navbar/Navbar';
-import Payment from './pages/Payment/Payment';
-import Footer from './components/Footer/Footer';
+
 
 
 
@@ -12,7 +10,7 @@ function App() {
 
   return (
     <>
-    <Navbar/>  
+    
     <Home/>
     <ProductOverview/>
 
