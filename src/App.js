d381832fd641b4a5084b181e6c81ac028b88c4d5
Duleesha28455 <duleeshajayangani29@gmail.com>
2024-11-30 13:46:28 +0530

    Edit ProductOverview page and app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import './App.css';
 import Home from './pages/Home/Home';
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
-
-
-
+import ProductOverview from './pages/ProductOverview/ProductOverview';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  
-
   return (
     <>
-
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/home' element={<Home/>}></Route>
-      <Route path='/login' element={<Login/>}></Route>
-    </Routes>
-    </BrowserRouter>
-
-
-    
-    
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/product-overview' element={<ProductOverview />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
